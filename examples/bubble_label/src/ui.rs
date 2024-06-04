e198a0b906bea78246ef9e556a1072da0abcd110
noxware <7684329+noxware@users.noreply.github.com>
2024-06-04 15:10:59 -0300

    not reusable bubble label

---
+++ examples/bubble_label/src/ui.rs
@@ -0,0 +1,41 @@
+use makepad_widgets::*;
+
+live_design!(
+    import makepad_widgets::base::*;
+    import makepad_widgets::theme_desktop_dark::*;
+    import crate::bubble_label::*;
+
+    Ui = {{Ui}} {
+        body = <View> {
+            padding: {left: 10.0, right: 10.0, top: 28.0, bottom: 10.0},
+            width: Fill,
+            height: Fill,
+
+            <RoundedView> {
+                padding: 8,
+                width: Fill,
+                height: Fill,
+                draw_bg: {
+                    color: #066,
+                }
+                <BubbleLabel> {}
+            }
+        }
+    }
+);
+
+#[derive(Live, LiveHook, Widget)]
+pub struct Ui {
+    #[deref]
+    deref: Window,
+}
+
+impl Widget for Ui {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
+        self.deref.handle_event(cx, event, scope);
+    }
+
+    fn draw_walk(&mut self, cx: &mut Cx2d, scope: &mut Scope, walk: Walk) -> DrawStep {
+        self.deref.draw_walk(cx, scope, walk)
+    }
+}
